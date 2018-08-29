20b816b88ef6b3f9a2a6d96a4c55de9f56e4b617
Sofia Arciniegas <sofiarciniegas17@gmail.com>
2018-08-29 15:23:31 -0500

    orin

--- app/src/main/java/com/worldskills/myapplication/Home.java
+++ app/src/main/java/com/worldskills/myapplication/Home.java
@@ -1,13 +1,25 @@
 package com.worldskills.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Home extends AppCompatActivity {
+    public static final String DIFICULTAD="DIFICULTAD";
+    public static final String TIEMPO= "TIEMPO";
+    public static final String MODO = "MODO";
+    public static final String PLAYER1 = "PLAYER1";
+    public static final String PLAYER2= "PLAYER2";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void partida(View view) {
+        Intent intent= new Intent(Home.this, Partida.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/worldskills/myapplication/Splash.java
+++ app/src/main/java/com/worldskills/myapplication/Splash.java
@@ -1,7 +1,10 @@
 package com.worldskills.myapplication;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class Splash extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class Splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+
+        ImageView imageView= findViewById(R.id.image);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent= new Intent(Splash.this, Home.class);
+                intent.putExtra("ingreso", true);
+                startActivity(intent);
+            }
+        }, 3000);
+
+
     }
 }
